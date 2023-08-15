7aaf5e029a3dbca03cc431fc8b4821a2e27be3e9
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-08-15 22:35:53 +0700

    Add requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(setup_requires=["setuptools_scm"], include_package_data=True)
+    setuptools.setup(
+        setup_requires=["setuptools_scm"], 
+        install_requires=[
+            "transformers==4.28.0",
+            "accelerate"
+        ],
+        include_package_data=True
+    )
