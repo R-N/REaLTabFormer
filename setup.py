fc09906d95ebf7ff7e6c22e5ef973682bf122414
Muhammad Rizqi Nur <rizqinur2010@gmail.com>
2023-08-27 22:16:41 +0700

    Update reqs

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ if __name__ == "__main__":
         setup_requires=["setuptools_scm"], 
         install_requires=[
             "transformers==4.28.0",
-            "accelerate>=0.20.3"
+            "accelerate>=0.20.3",
+            "datasets>=2.6.1",
+            "numpy>=1.21.6",  # "numpy >= 1.23.4",
+            "pandas>=1.3.5",  # "pandas >= 1.5.1",
+            "scikit_learn>=1.0.2",  # "scikit_learn >= 1.1.3",
+            "torch>=1.13.0",
+            "tqdm>=4.64.1",
+            "shapely>=1.8.5.post1",
         ],
         include_package_data=True
     )
